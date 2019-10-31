cd6dc5397b4c8024caa184935ee07c42252c3e8c
John Long <jlcomp45@gmail.com>
2019-10-31 10:53:04 -0500

    completed all but filter() exercise - kept getting empty array

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -304,13 +304,41 @@
 // ***array methods practice 
 
 const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+function checkLetter() {
+    planets.forEach(planet => {
+        if (planet.includes("e")) {
+            return planet
+        }
+        console.log("planet in checkLetter", planet)
+    })
+}
+const ePlanets = planets.filter(checkLetter)
+console.log("e planets", ePlanets)
+/*
+    Use the filter method to create a new array that
+    contains planets with the letter 'e'. Use the `includes()`
+    method on strings.
 
+    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+*/
+const planetEl = document.getElementById("planets")
+planets.forEach(planet => {
+    planetEl.innerHTML += `
+    <p>${planet}</p>
+    `
+})
 /*
     Use the forEach method to add the name of each planet
     to a section element in your HTML with an id of "planets".
     Use string templates to construct the DOM elements.
 */
-const planetEl = document.getElementById("planets")
+// exercise completed - created section element in html with id planets, forEach to display in <p> tags to dom
+const capitalPlanets = planets.map(planets => {
+    console.log("mapped planets")
+    return planets.charAt(0).toUpperCase() + planets.slice(1)
+});
+console.log("capitals", capitalPlanets)
+// completed exercise - console logged to confirm.  used .slice to inclue rest of word and only capitalize 1st letter - initial code capitalized all letters
 
 /*
     Use the map method to create a new array where the
@@ -320,11 +348,3 @@ const planetEl = document.getElementById("planets")
     https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
 */
 
-
-/*
-    Use the filter method to create a new array that
-    contains planets with the letter 'e'. Use the `includes()`
-    method on strings.
-
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/includes
-*/
